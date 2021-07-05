7024ea3aa58e290cd891519735ca1429aa7459e7
honma <unity1017@gmail.com>
2021-07-06 00:53:56 +0900

    init

---
+++ main.js
@@ -0,0 +1,58 @@
+
+function GenerateAnagram(message)
+{
+    if(message.length < 2) return [message];
+
+    // generate serial numbers
+    let serials = new Array(message.length);
+    for(let i = 0; i < message.length; ++i) serials[i] = i;
+    
+    // shuffle Fisher-Yates
+    let i = serials.length;
+    while(i){
+        let j = Math.floor(Math.random() * i);
+        let t = serials[--i];
+        serials[i] = serials[j];
+        serials[j] = t;
+    }
+
+    // swap characters
+    let swapIdxs = [serials[0],serials[1]];
+    let tmpMessage = message.split('');
+    let tmp = tmpMessage[swapIdxs[0]];
+    tmpMessage[swapIdxs[0]] = tmpMessage[swapIdxs[1]];
+    tmpMessage[swapIdxs[1]] = tmp;
+    message = tmpMessage.join('');
+
+    return message;
+}
+
+const App = {
+    data() {
+      return {
+        message: "",
+        warnMessage: "",
+        items: []
+      }
+    },
+    methods: {
+      GenerateAnagrams(event) {
+
+        if(this.message.length < 2){
+            this.warnMessage = "2文字以上入力してください。";
+            return;
+        }
+        // clear items  
+        this.items.length = 0;
+        
+        // generate anagrams
+        for(let i = 0; i < 100; ++i) {
+            let anagram = GenerateAnagram(this.message);
+            this.items.push(anagram);    
+         }
+      }
+    }
+  }
+  
+
+  Vue.createApp(App).mount('#counter')
